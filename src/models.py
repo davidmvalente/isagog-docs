32ceb475320f7e9869e80b8129e401fe7af1e4e7
David Valente <d.valente@davidvalente.xyz>
2025-06-12 19:04:32 +0200

    add len to composite objects

--- src/models.py
+++ src/models.py
@@ -14,8 +14,14 @@ class Relation(BaseModel):
 class Relations(BaseModel):
     relations: List[Relation]
 
+    def __len__(self):
+        return len(self.relations)  
+
 class Entities(BaseModel):
     entities: List[Entity]
+
+    def __len__(self):
+        return len(self.entities)
 
 class KnowledgeStub(BaseModel):
     entities: Entities
